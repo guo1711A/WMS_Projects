e8fe538f7b63cca9ad92968ac17556adff61444d
Nick0629 <1120756858@qq.com>
2020-07-21 14:34:44 +0800

    登录页面

---
+++ MVC_WMS_Project/MVC_WMS_Project/Controllers/Nick_Controllers/NickController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_WMS_Project.Controllers.Nick_Controllers
+{
+    public class NickController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
